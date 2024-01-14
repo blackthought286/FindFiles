77da98b924d0a8bc536e2d8d8fb024a4f90659e4
Everett Bryant <blackthought286@gmail.com>
2024-01-14 05:21:34 -0500

    using custom template and adding initial options to FindFiles project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FindFiles;
+using CommandLine;
+using CommandLine.Text;
+
+var results = Parser.Default.ParseArguments<Options>(args)
+    .WithParsed<Options>(options =>
+    {
+        WriteLine($"Text: {options.Text}");
+    });
+
+results.WithNotParsed(_ => WriteLine(HelpText.RenderUsageText(results)));
